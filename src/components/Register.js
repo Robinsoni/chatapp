aac01d651c99765d3ad818746e568fb8ef2be30f
Robin Soni <sonirobin369@gmail.com>
2022-09-16 14:24:36 +0530

    Register page added

---
+++ src/components/Register.js
@@ -0,0 +1,23 @@
+import avatar from '../assets/img/addAvatar.png';
+const Register = () => {
+    return (
+        <form className="register">
+            <div className="container">
+                <h3>Robin Chat</h3>
+                    Register
+                <input type="text" className="username" placeholder="Username"/>
+                <input type="email" className="email" placeholder="Email"/>
+                <input type="password" className="password"  placeholder="Password"/>
+                <input type="file"   className="file" id="file" />
+                
+                <label htmlFor="file" className='avtar' >
+                    <img height="30px" src={avatar} alt="avatar" />
+                    <span>Add an avatar</span>
+                </label>   
+                <button type="submit">Sign up</button>
+                You do have an account? Login
+            </div>
+        </form>
+    );
+};
+export default Register;
